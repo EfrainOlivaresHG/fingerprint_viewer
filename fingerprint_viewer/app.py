2adcbd21d9acaf35b8c72a3620873fba7418ce87
EfrainOlivaresHG <efrain.olivares@hgdata.com>
2017-08-30 21:12:15 -0700

    working with 2 fingerprints

--- fingerprint_viewer/app.py
+++ fingerprint_viewer/app.py
@@ -14,20 +14,16 @@ conf = {
 word_dict = rv.load_word_location_dict_from_file(conf["map_path_file"])
 word_vectors = rv.load_location_words_dict_from_file(conf["map_path_loc_file"])
 retina = rv.Retina(['aws'], word_dict, word_vectors, None)
- 
-
 
 @app.route('/')
-def hello():
-    return render_template('line_basic.html')
-
-@app.route('/bubble')
 def bubble():
-    return render_template('bubble_basic.html')
+    with open(conf["text_corpus"],'r') as source:
+        url_list = [line.split('\t')[0].strip() for line in source]
+    return render_template('bubble_basic.html', url_list=url_list)
 
-@app.route('/getretina', methods=['GET', 'POST'])
-def get_retina():
-    print "Looking for {}".format('lon') 
+@app.route('/getretina/<urlname>', methods=['GET', 'POST'])
+def get_retina(urlname):
+    print "Looking for {}".format(urlname) 
     text = ['aws']
     with open(conf["text_corpus"], 'r') as source:
         for line in source:
@@ -35,7 +31,7 @@ def get_retina():
             binary = cols[2].strip()
             url = cols[0].strip()
             text = [word for word in cols[1].strip().split()]
-            if url == "pennyful.in":
+            if url == urlname:
                 print "FOUND IT!"
                 break
 
@@ -44,7 +40,6 @@ def get_retina():
     for tup in tups:
         data.append({ "x": tup[0], "y": tup[1] })
     jdatas = json.dumps(data)
-    # print jdatas
     return jdatas
 
 if __name__ == '__main__':
